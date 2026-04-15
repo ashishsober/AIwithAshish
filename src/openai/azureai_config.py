54cd057a3c09537ac1cc89fffcbe35f6be4f814e
Gupta, Ashish <gupta.ashish@optum.com>
2026-04-15 15:58:26 +0530

    remove endpoint

--- src/openai/azureai_config.py
+++ src/openai/azureai_config.py
@@ -5,10 +5,10 @@ from openai import AzureOpenAI
 load_dotenv()
 
 
-endpoint = "https://hcep-open-ai-as-a-svc.openai.azure.com/"
 model_name = "gpt-4o-mini"
 DEPLOYMENT = "gpt-4o-mini"
 
+endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 subscription_key = os.getenv("AZURE_OPENAI_API_KEY")
 api_version = "2024-12-01-preview"
 
